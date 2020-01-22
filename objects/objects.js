feaeffba0a87503b19bec8345e2eac41ab884429
Alex Lee <alex@uniteus.com>
2020-01-21 20:49:00 -0500

    Objects and varaibles

---
+++ objects/objects.js
@@ -0,0 +1,37 @@
+// const dog = {
+//   name: 'Happy',
+//   age: 1,
+//   adopted: true,
+// };
+
+// console.log(dog);
+// console.log(dog.name);
+// console.log(dog.age);
+// console.log(dog.adopted);
+
+// const dog2 = {
+//   name: 'Jack',
+//   age: 3,
+//   adopted: false,
+//   details: {
+//     description: 'Current not adopted.  Pending Approval',
+//     healthy: true,
+//   }
+// }
+
+// // console.log(dog2);
+// console.log(dog2.details);
+// console.log(dog2.details.description);
+// console.log(dog2.details.healthy);
+
+const dog2 = {
+  name: 'Jack',
+  age: 3,
+  adopted: false,
+  description: 'Current not adopted.  Pending Approval',
+  healthy: true,
+}
+
+// console.log(dog2);
+console.log(dog2.description);
+console.log(dog2.healthy);
---
+++ variables/variables.js
@@ -0,0 +1,26 @@
+// cmd + / (comment out code)
+// let x;
+
+// console.log(x)
+
+// x = 1;
+
+// console.log(x)
+
+
+// Redefine a variable using the keyword "let"
+// let x = 1; // Number type
+
+// console.log(x);
+
+// x = "This is a string" // String Type
+
+// console.log(x);
+
+// Define a variable using the keyword const
+
+const x = 1;
+console.log(x);
+
+x = 3;
+console.log(x);
